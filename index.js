07436a47fe2345c0bc5f7ec6087cec1fadb0316e
Manish <mvmastar@enacton.com>
2022-10-11 12:09:12 +0530

    color picker basic functionality complete

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const colorPickerBtn = document.querySelector(".btn");
+const selColor = document.querySelector(".selectedColor");
+const selValue = document.querySelector(".selectedValue");
 
 colorPickerBtn.addEventListener("click", async () => {
-  //   console.log("clicked");
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  //   console.log(tab);
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: pickYourColor,
-  });
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: pickYourColor,
+    },
+    async (color) => {
+      const [data] = await color;
+      const gridColor = data.result.sRGBHex;
+      console.log(data);
+      if (data.result) {
+        selColor.style.backgroundColor = gridColor;
+        selValue.innerHTML = gridColor;
+      }
+    }
+  );
 });
+
+const pickYourColor = async () => {
+  try {
+    const eyeDropper = new EyeDropper();
+    const response = await eyeDropper.open();
+    return response;
+  } catch (error) {}
+};
